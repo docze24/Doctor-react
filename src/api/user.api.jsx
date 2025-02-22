3adef44dcd8403f6e30b0009c70baa8933e9f25c
Hariom <doczehariom@gmail.com>
2025-02-22 11:01:16 +0530

    API implementation in Add user Edit user

--- src/api/user.api.jsx
+++ src/api/user.api.jsx
@@ -1,25 +1,22 @@
 import axiosInstance from './axiosInstance';
 
-const createUser = async (data) => {
-  try {
-    const response = await axiosInstance.post('doctor-user', data);
-    return response.data; // You can return or handle the response data here
-  } catch (error) {
-    console.error('Error creating user:', error.response || error.message);
-    throw error; // Optionally re-throw the error to be handled elsewhere
-  }
-};
 
- //const editUser = (data)=> axiosInstance.post('user/edit',data);
- //const getUsers = (data)=> axiosInstance.get('doctor-user',data);
+const createUser = (data) => axiosInstance.post('/doctor-user', data);
+
+const editUser = (data) => axiosInstance.put(`/doctor-user/${data.userId}`, data);
+
+const getUserById = (userId) => axiosInstance.get(`/doctor-user/${userId}`);
 
  const getUsers = (params) => {
     const queryString = new URLSearchParams(params).toString();
     return axiosInstance.get(`doctor-user?${queryString}`);
   };
+
  export default {
     getUsers,
-    createUser
+    createUser,
+    editUser,
+    getUserById
    
   };
   
